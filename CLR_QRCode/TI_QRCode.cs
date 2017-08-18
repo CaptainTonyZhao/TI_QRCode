3b17de06fd7d03ea4e1234d8b54d8628c43f0862
Adam Wu <adam.wu@trinfo.net>
2017-08-18 15:36:43 +0800

    Init Commit

---
+++ CLR_QRCode/TI_QRCode.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace TI_QRCode
+{
+    public class TI_QRCode
+    {
+        [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
+        public static SqlBinary GenQRCode(string source, int width, int height)
+        {
+            EncodingOptions options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = width,
+                Height = height
+            };
+            BarcodeWriter write = new BarcodeWriter();
+            write.Format = BarcodeFormat.QR_CODE;
+            write.Options = options;
+            Bitmap bitmap = write.Write(source);
+
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Jpeg);
+            byte[] arr = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(arr, 0, (int)ms.Length);
+            ms.Close();
+            return arr;
+        }
+    }
+}
